e8b99e5801aa08889dafe87201afeedb566ac625
Caua Mattos - Casa <caua.h4k@gmail.com>
2025-03-03 23:38:55 -0300

    feat: added delete note feature

--- src/main/java/com/nebula/backend/nebulabackend/controller/NoteController.java
+++ src/main/java/com/nebula/backend/nebulabackend/controller/NoteController.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("notes")
@@ -56,6 +58,17 @@ public class NoteController {
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Note> deleteNote(@PathVariable UUID id) {
+        try {
+            noteService.deleteNoteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
