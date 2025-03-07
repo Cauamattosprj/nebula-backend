2c8812e6e2c117fd3fe635a1e8cdb64cf0e4201b
Cauã Mattos <caua.h4k@protonmail.com>
2025-03-07 13:39:32 -0300

    feat: added findByTitle method

--- src/main/java/com/nebula/backend/nebulabackend/repository/FolderRepository.java
+++ src/main/java/com/nebula/backend/nebulabackend/repository/FolderRepository.java
@@ -1,5 +1,6 @@
 package com.nebula.backend.nebulabackend.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nebula.backend.nebulabackend.model.Folder;
 
 public interface FolderRepository extends JpaRepository<Folder, UUID> {
-
+    Optional<Folder> findByTitle(String title); 
 }
