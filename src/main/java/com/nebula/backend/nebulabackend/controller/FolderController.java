5afe12788277b64b6cfcf1aa27bd3f5789dc76dd
Cauã Mattos <caua.h4k@protonmail.com>
2025-03-07 12:53:18 -0300

    feat: created folder controller and folder service

---
+++ src/main/java/com/nebula/backend/nebulabackend/controller/FolderController.java
@@ -0,0 +1,60 @@
+package com.nebula.backend.nebulabackend.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nebula.backend.nebulabackend.model.ApiResponse;
+import com.nebula.backend.nebulabackend.model.Folder;
+import com.nebula.backend.nebulabackend.service.FolderService;
+
+@RestController
+@RequestMapping("folders")
+@CrossOrigin("*")
+public class FolderController {
+    private FolderService folderService;
+
+    public FolderController(FolderService folderService) {
+        this.folderService = folderService;
+    }
+
+    @GetMapping()
+    public List<Folder> getAllFolders() {
+        return folderService.getAllFolders();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<Optional<Folder>> getFolderById(@PathVariable UUID id) {
+        Optional<Folder> folder = folderService.getFolderById(id);
+        return ApiResponse.success(folder);
+    }
+
+    @PutMapping("/{id}")
+    public ApiResponse<Folder> updateFolder(@PathVariable UUID id, @RequestBody Folder updatedFolder) {
+        Folder folder = folderService.updateFolder(id, updatedFolder);
+        return ApiResponse.success(folder);
+    }
+
+    @PostMapping()
+    public ApiResponse<Folder> createFolder(@RequestBody Folder newFolder) {
+        Folder folder = folderService.createFolder(newFolder);
+        return ApiResponse.success(folder);
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<Optional<Folder>> deleteFolderById(@PathVariable UUID id) {
+        Optional<Folder> folder = folderService.getFolderById(id);
+        folderService.deleteFolderById(id);
+        return ApiResponse.success(folder);
+    }
+}
---
+++ src/main/java/com/nebula/backend/nebulabackend/service/FolderService.java
@@ -0,0 +1,52 @@
+package com.nebula.backend.nebulabackend.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.nebula.backend.nebulabackend.exception.NotFoundException;
+import com.nebula.backend.nebulabackend.model.Folder;
+import com.nebula.backend.nebulabackend.repository.FolderRepository;
+
+@Service
+public class FolderService {
+    private FolderRepository folderRepository;
+
+    public FolderService(FolderRepository folderRepository) {
+        this.folderRepository = folderRepository;
+    }
+
+    public Folder createFolder(Folder folder) {
+        return folderRepository.save(folder);
+    }
+
+    public List<Folder> getAllFolders() {
+        return folderRepository.findAll();
+    }
+
+    public Optional<Folder> getFolderById(UUID id) {
+        if (!folderRepository.existsById(id)) {
+            throw new NotFoundException(id);
+        }
+        return folderRepository.findById(id);
+    }
+
+    public Folder updateFolder(UUID id, Folder folderUpdates) {
+        return folderRepository.findById(id).map(folder -> {
+            if (folderUpdates.getTitle() != null && !folderUpdates.getTitle().isEmpty()) {
+                folder.setTitle(folderUpdates.getTitle());
+            }
+
+            return folderRepository.save(folder);
+        }).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    public void deleteFolderById(UUID id) {
+        if (!folderRepository.existsById(id)) {
+            throw new NotFoundException(id);
+        }
+        folderRepository.deleteById(id);
+    }
+}
