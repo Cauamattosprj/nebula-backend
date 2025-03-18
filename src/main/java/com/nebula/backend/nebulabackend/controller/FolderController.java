d8d9863135ff02658f78a70beef829ff6d400955
Cauã Mattos <caua.h4k@protonmail.com>
2025-03-18 09:10:45 -0300

    feat: updated the api endpoints route

--- src/main/java/com/nebula/backend/nebulabackend/controller/FolderController.java
+++ src/main/java/com/nebula/backend/nebulabackend/controller/FolderController.java
@@ -19,7 +19,7 @@ import com.nebula.backend.nebulabackend.model.Folder;
 import com.nebula.backend.nebulabackend.service.FolderService;
 
 @RestController
-@RequestMapping("folders")
+@RequestMapping("/api/folders")
 @CrossOrigin("*")
 public class FolderController {
     private FolderService folderService;
@@ -28,33 +28,33 @@ public class FolderController {
         this.folderService = folderService;
     }
 
-    @GetMapping()
+    @GetMapping("/list")
     public ApiResponse<List<Folder>> getAllFolders() {
         List<Folder> folders = folderService.getAllFolders();
         return ApiResponse.success(folders);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/list/{id}")
     public ApiResponse<Optional<Folder>> getFolderById(@PathVariable UUID id) {
         Optional<Folder> folder = folderService.getFolderById(id);
         return ApiResponse.success(folder);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ApiResponse<Folder> updateFolder(@PathVariable UUID id, @RequestBody Folder updatedFolder) {
         Folder folder = folderService.updateFolder(id, updatedFolder);
         return ApiResponse.success(folder);
     }
 
-    @PostMapping()
+    @PostMapping("/create")
     public ApiResponse<Folder> createFolder(@RequestBody Folder newFolder) {
         Folder folder = folderService.createFolder(newFolder);
         return ApiResponse.success(folder);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ApiResponse<String> deleteFolderById(@PathVariable UUID id) {
         folderService.deleteFolderById(id);
-        return ApiResponse.success("Folder deleted");
+        return ApiResponse.success(String.format("Folder with id %s deleted", id));
     }
 }
