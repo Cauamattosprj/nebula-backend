6568fb4a7009e5469b4ba7e0e640e1ba5188789b
Caua Mattos - Casa <caua.h4k@gmail.com>
2025-04-26 18:52:14 -0300

    feat: implemented findAllTitles query

--- src/main/java/com/nebula/backend/nebulabackend/repository/FolderRepository.java
+++ src/main/java/com/nebula/backend/nebulabackend/repository/FolderRepository.java
@@ -1,12 +1,17 @@
 package com.nebula.backend.nebulabackend.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nebula.backend.nebulabackend.model.Folder;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FolderRepository extends JpaRepository<Folder, UUID> {
-    Optional<Folder> findByTitle(String title); 
+    Optional<Folder> findByTitle(String title);
+
+    @Query("SELECT folder.title FROM Folder folder")
+    List<String> findAllTitles();
 }
--- src/main/java/com/nebula/backend/nebulabackend/repository/NoteRepository.java
+++ src/main/java/com/nebula/backend/nebulabackend/repository/NoteRepository.java
@@ -1,12 +1,17 @@
 package com.nebula.backend.nebulabackend.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nebula.backend.nebulabackend.model.Note;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoteRepository extends JpaRepository<Note, UUID> {
     Optional<Note> findByTitle(String title);
+
+    @Query("SELECT note.title from Note note")
+    List<String> findAllTitles();
 }
